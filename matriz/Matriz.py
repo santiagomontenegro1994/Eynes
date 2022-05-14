9f2c12f4b9d7df8c5735725b5d8b6212ed8e08a0
Pachy <pachycat94@gmail.com>
2022-05-14 19:15:28 -0300

    creo el metodo para recorrecls

--- matriz/Matriz.py
+++ matriz/Matriz.py
@@ -1,7 +1,114 @@
+from asyncio.windows_events import NULL
+from itertools import count
+from pickle import TRUE
 import random
 import numpy as np
 
 
-matriz= np.round(100 * np.random.random([5,5])) 
-
+matriz= 2 * np.random.random([5,5]) #Creo la matriz de 5 x 5 con numero aleatoreos(lo multiplico x 100 por que por defecto genera numeros entre 0 y 1)
+matriz= matriz.astype(int) #convierto el contenido de la matriz en numeros enteros
 print(matriz)
+
+def sec4Numeros(matriz):
+    
+    respuesta='sin secuencia'
+    posicionInicial=NULL
+    posicionFinal=NULL
+    j=0 #auxiliar para saber el indice de la matriz 
+    k=0 #auxiliar para saber si ya hubo una secuencia 
+     
+    for c in range(len(matriz[1,:])): #recorro cada una de las columnas de la matriz
+               
+        tuplaAux=np.unique(matriz[:,c], return_counts=True, return_index=True)
+        #con la columna que recorre, creo una tupla que contiene 3 listas
+        #l primera tiene los elementos, la segunda contiene el indice donde se ubica, la tercera tiene la cantidad de veces que se repite.
+        
+        i=0 #auxiliar para saber el subindice de la matriz
+        j=j+1
+        
+        print(tuplaAux)
+                  
+        for x in tuplaAux[2]: #recorro la lista de cantidad de veces que se repite un elemento
+            i=i+1
+            
+            if (x>3) and ((matriz[1,c])==tuplaAux[0][i-1]) and ((matriz[2,c])==tuplaAux[0][i-1]) and ((matriz[3,c])==tuplaAux[0][i-1]):
+                # si hay algun elemento que se repite es mas de 3 
+                # y que no tenga otro elemento en medio de la columna para corroborar que es secuencia
+                if k<1:  
+                    posicionInicial= str(tuplaAux[1][i-1]) + ' ' + str(j-1)
+                    posicionFinal=str(tuplaAux[1][i-1] + x-1) + ' ' + str(j-1)
+                    print(posicionInicial)
+                    print(posicionFinal)
+                    k=k+1 #Cambio auxiliar para saber que ya se encontro una secuencia
+                    respuesta='Secuencia encontrada en posiciones: \n' + posicionInicial + ' , ' + posicionFinal    
+                else:
+                    posicionInicial= str(tuplaAux[1][i-1]) + ' ' + str(j-1)
+                    posicionFinal=str(tuplaAux[1][i-1] + x-1) + ' ' + str(j-1)
+                    print(posicionInicial)
+                    print(posicionFinal)
+                    respuesta= respuesta + '\n' + posicionInicial + ' , ' + posicionFinal
+                    
+#-------------------ahora hago lo mismo para revisar cada fila de la matriz---------------------# 
+               
+    #primero reseteo los contadores
+    j=0 #auxiliar para saber el indice de la matriz 
+    
+    for l in range(len(matriz[:,1])): #recorro cada una de las listas de la matriz
+               
+        tuplaAux=np.unique(matriz[l,:], return_counts=True, return_index=True)
+        #con la lista que recorre, creo una tupla que contiene 3 listas
+        #la primera tiene los elementos, la segunda contiene el indice donde se ubica, la tercera tiene la cantidad de veces que se repite.
+        
+        i=0 #auxiliar para saber el subindice de la matriz
+        j=j+1
+        
+        print(tuplaAux)
+                  
+        for x in tuplaAux[2]: #recorro la lista de cantidad de veces que se repite un elemento
+            i=i+1
+            
+            if (x>3) and ((matriz[l,1])==tuplaAux[0][i-1]) and ((matriz[l,2])==tuplaAux[0][i-1]) and ((matriz[l,3])==tuplaAux[0][i-1]):
+                # si hay algun elemento que se repite es mas de 3 
+                # y que no tenga otro elemento en medio de la lista para corroborar que es secuencia
+                if k<1:  
+                    posicionInicial= str(j-1) + ' ' + str(tuplaAux[1][i-1])
+                    posicionFinal=str(j-1) + ' ' + str(tuplaAux[1][i-1] + x-1)  
+                    print(posicionInicial)
+                    print(posicionFinal)
+                    k=k+1 #Cambio auxiliar para saber que ya se encontro una secuencia
+                    respuesta='Secuencia encontrada en posiciones: \n' + posicionInicial + ' , ' + posicionFinal    
+                else:
+                    posicionInicial= str(j-1) + ' ' + str(tuplaAux[1][i-1])
+                    posicionFinal=str(j-1) + ' ' + str(tuplaAux[1][i-1] + x-1)
+                    print(posicionInicial)
+                    print(posicionFinal)
+                    respuesta= respuesta + '\n' + posicionInicial + ' , ' + posicionFinal
+    
+    
+
+    
+    
+    
+    return print(respuesta)     
+                
+print(sec4Numeros(matriz))        
+    
+              
+        
+        
+                
+             
+            
+        
+            
+        
+       
+        
+    
+        
+    
+        
+        
+    
+    
+
